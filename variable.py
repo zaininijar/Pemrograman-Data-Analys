554af1ac6c371a6eb9857f89e8b5e9466ce85629
zaininijar <zaininijar7610@gmail.com>
2024-03-26 11:51:25 +0700

    basic variable

---
+++ variable.py
@@ -0,0 +1,21 @@
+nama = "Ahmad Zaini Nijar"
+print(f"Nama saya {nama}")
+
+#! variable numeriv
+umur = 21
+print(f"Umur saya {umur}")
+
+#! variable boolean
+sudah_menikah = False
+status = [
+    {
+        "status": False,
+        "description": "Sudah Menikah"
+    },
+    {
+        "status": False,
+        "description": "Belum Menikah"
+    }
+]
+
+print(status[0]['status'] == sudah_menikah)
